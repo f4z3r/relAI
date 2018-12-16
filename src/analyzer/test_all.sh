91b27a4ce64405d8ed0df48ccc5424e84a57f6f1
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-12-16 22:17:41 +0100

    Added new automation test scripts

---
+++ src/analyzer/test_all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+./test.sh mnist_relu_3_10 0.1
+./test.sh mnist_relu_3_20 0.1
+./test.sh mnist_relu_3_50 0.1
+./test.sh mnist_relu_6_20 0.1
+./test.sh mnist_relu_6_50 0.1
+./test.sh mnist_relu_6_100 0.1
+./test.sh mnist_relu_6_200 0.1
+./test.sh mnist_relu_9_100 0.1
+./test.sh mnist_relu_9_200 0.1
+./test.sh mnist_relu_4_1024 0.001
